69f1c9c2bac82c26a53022a4673a19654ac2b49b
Shane Claussen <sclaussen@yahoo.com>
2020-03-06 17:51:02 -0800

    Added algorithm & readme

---
+++ README.md
@@ -0,0 +1,29 @@
+This algorithm is intended to be used to provide equal playing time
+for youth sports.  It takes a list of player names and the number of
+segments a game is broken into and then generates an equitable playing
+time matrix for the players spread out over the playing segments.
+
+For example, assume you have 7 total players for a game and 8
+opportunities in the game for wholesale substitutions and the youth
+sports league requires equal playing time:
+
+```
+$ node index.js "Caden Jordan Landon Kai Evan Hayato Leandro" 8
+1: Caden Evan Hayato Jordan Kai (Out: Landon Leandro)
+2: Landon Leandro Caden Evan Hayato (Out: Jordan Kai)
+3: Jordan Kai Landon Leandro Caden (Out: Evan Hayato)
+4: Evan Hayato Jordan Kai Landon (Out: Leandro Caden)
+5: Leandro Caden Evan Hayato Jordan (Out: Kai Landon)
+6: Kai Landon Leandro Caden Evan (Out: Hayato Jordan)
+7: Hayato Jordan Kai Landon Leandro (Out: Caden Evan)
+8: Caden Evan Hayato Jordan Kai (Out: Landon Leandro)
+
+          1 2 3 4 5 6 7 8
+Caden     X X X   X X   X  6
+Evan      X X   X X X   X  6
+Hayato    X X   X X   X X  6
+Jordan    X   X X X   X X  6
+Kai       X   X X   X X X  6
+Landon      X X X   X X    5
+Leandro     X X   X X X    5
+```
---
+++ index.js
@@ -0,0 +1,188 @@
+var _ = require('lodash');
+
+
+
+//     players: [
+//         {
+//             name: player1
+//             segmentStatus: [ 1: in, 2: out, ... ]
+//             segmentsIn: 5
+//         }
+//         {
+//             ...
+//         }
+//         ...
+//     ]
+//
+//
+//     segments: [
+//         {
+//             name: 1
+//             in: [ player1, player3, ... ]
+//             out: [ player2, ... ]
+//         }
+//         {
+//             ....
+//         }
+//         ...
+//     ]
+var players = [];
+var segments = [];
+
+
+
+// Tests
+// node index.js "P01 P02 P03 P04 P05" 4
+// node index.js "P01 P02 P03 P04 P05" 8
+// node index.js "P01 P02 P03 P04 P05 P06" 4
+// node index.js "P01 P02 P03 P04 P05 P06" 8
+// node index.js "P01 P02 P03 P04 P05 P06 P07" 4
+// node index.js "P01 P02 P03 P04 P05 P06 P07" 8
+// node index.js "P01 P02 P03 P04 P05 P06 P07 P08" 4
+// node index.js "P01 P02 P03 P04 P05 P06 P07 P08" 8
+// node index.js "P01 P02 P03 P04 P05 P06 P07 P08 P09" 4
+// node index.js "P01 P02 P03 P04 P05 P06 P07 P08 P09" 8
+// node index.js "P01 P02 P03 P04 P05 P06 P07 P08 P09 P10" 4
+// node index.js "P01 P02 P03 P04 P05 P06 P07 P08 P09 P10" 8
+// node index.js "Caden Jordan Landon Kai Evan" 8
+// node index.js "Caden Jordan Landon Kai Evan Hayato" 8
+// node index.js "Caden Jordan Landon Kai Evan Hayato Leandro" 8
+// node index.js "Caden Jordan Landon Kai Evan Hayato Leandro Hayden" 8
+// node index.js "Caden Jordan Landon Kai Evan Hayato Leandro Hayden Luke" 8
+// node index.js "Caden Jordan Landon Kai Evan Hayato Leandro Hayden Luke P11" 8
+// node index.js "Caden Jordan Landon Kai Evan Hayato Leandro Hayden Luke P11 P12" 8
+main(process.argv);
+
+
+function main(args) {
+    let playerNames = [];
+    for (let playerName of args[2].split(' ')) {
+        playerNames.push(playerName);
+    }
+
+    playingTime(playerNames, parseInt(args[3]));
+}
+
+
+function playingTime(playerNames, totalSegments) {
+
+    initializePlayers(playerNames);
+    initializeSegments(totalSegments);
+
+    for (let segment of segments) {
+        processSegment(segment);
+    }
+
+    printBySegment();
+    console.log();
+    printPlayerSegmentMatrix();
+}
+
+
+function initializePlayers(playerNames) {
+    for (let playerName of playerNames) {
+        players.push({
+            name: playerName,
+            segmentStatus: [],
+            segmentsIn: 0
+        });
+    }
+
+    players = _.orderBy(players, [ 'name' ], [ 'asc' ]);
+}
+
+
+function initializeSegments(totalSegments) {
+    for (let i = 0; i < totalSegments; i++) {
+        let segmentNumber = i + 1;
+        segments.push({
+            name: segmentNumber.toString(),
+            in: [],
+            out: []
+        });
+    }
+}
+
+
+function processSegment(segment) {
+
+    let playerNumber = 1;
+    for (let player of _.orderBy(players, [ 'segmentsIn' ], [ 'asc' ])) {
+
+        // First 5 players in the ordered list play, others are out
+        if (playerNumber <= 5) {
+
+            // Update player's "segmentStatus" array
+            let status = {};
+            status[segment.name] = 'in';
+            player.segmentStatus.push(status);
+
+            // Update player's "segmentsIn" count
+            player.segmentsIn = player.segmentsIn + 1;
+
+            // Update segment's "in" array
+            segment.in.push(player.name);
+
+        } else {
+
+            // Update player's "segmentStatus" array
+            let status = {};
+            status[segment.name] = 'out';
+            player.segmentStatus.push(status);
+
+            // Update segment's "out" array
+            segment.out.push(player.name);
+        }
+
+        playerNumber++;
+    }
+}
+
+
+function printBySegment() {
+    for (let segment of segments) {
+        process.stdout.write(segment.name + ': ');
+        for (let player of segment.in) {
+            process.stdout.write(player + ' ');
+        }
+
+        if (segment.out.length > 0) {
+            process.stdout.write('(Out:');
+            for (let player of segment.out) {
+                process.stdout.write(' ' + player);
+            }
+            process.stdout.write(')');
+        }
+
+        console.log();
+    }
+}
+
+
+function printPlayerSegmentMatrix() {
+    const maxNameLength = 10;
+    const horizontalWhitespace = 1;
+
+    // Header
+    process.stdout.write(' '.padEnd(maxNameLength));
+    for (let segment of segments) {
+        process.stdout.write(segment.name + ' '.padEnd(horizontalWhitespace));
+    }
+    console.log();
+
+    // Players
+    for (let player of _.orderBy(players, [ 'name' ], [ 'asc' ])) {
+        process.stdout.write(player.name.substring(0, maxNameLength).padEnd(maxNameLength))
+        for (let status of player.segmentStatus) {
+            if (_.values(status)[0] === 'in') {
+                process.stdout.write('X' + ' '.padEnd(horizontalWhitespace));
+            } else {
+                process.stdout.write(' ' + ' '.padEnd(horizontalWhitespace));
+            }
+        }
+
+        process.stdout.write(' '.padEnd(horizontalWhitespace) + player.segmentsIn);
+
+        console.log();
+    }
+}
